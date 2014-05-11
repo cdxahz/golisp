af73d76e57fc3eb1f6f4591963e61d2b04957d47
Aska <songv.liu@gmail.com>
2014-05-11 21:57:07 +0800

    add float support

--- lisp/eval.go
+++ lisp/eval.go
@@ -4,11 +4,46 @@ import (
 	"fmt"
 )
 
+type Number interface{}
+
+func isAllInt(a, b Number) bool{
+    _, ok_a := a.(int)
+    _, ok_b := b.(int)
+    return ok_a && ok_b
+}
+
+func intValue(a, b Number) (int, int){
+    val_a, _ := a.(int)
+    val_b, _ := b.(int)
+    return val_a, val_b
+}
+
+func float64Value(a, b Number) (float64, float64){
+    val_ai, ok_ai := a.(int)
+    val_bi, ok_bi := b.(int)
+
+    val_a, _ := a.(float64)
+    val_b, _ := b.(float64)
+    if ok_ai{
+        if ok_bi{
+            return float64(val_ai), float64(val_bi)
+        }else{
+            return float64(val_ai), val_b
+        }
+    }else{
+        if ok_bi{
+            return val_a, float64(val_bi)
+        } else{
+            return val_a, val_b
+        }
+    }
+}
+
 var table map[string]interface{}
 
-func Eval(ast *Node) int {
+func Eval(ast *Node) Number {
 	var op []byte
-	var left, right, result int
+	var left, right, result Number
 	if ast == nil {
 		panic("invalid ast")
 	}
@@ -16,7 +51,7 @@ func Eval(ast *Node) int {
 	if ast.root.Type == OP {
 		if ast.left != nil {
 			if ast.left.root.Type == NUMBER {
-				left = toInt(ast.left.root.Value)
+				left = toNumber(ast.left.root.Value)
 			} else if ast.left.root.Type == OP {
 				left = Eval(ast.left)
 			} else {
@@ -25,7 +60,7 @@ func Eval(ast *Node) int {
 		}
 		if ast.right != nil {
 			if ast.right.root.Type == NUMBER {
-				right = toInt(ast.right.root.Value)
+				right = toNumber(ast.right.root.Value)
 			} else if ast.right.root.Type == OP {
 				right = Eval(ast.right)
 			} else {
@@ -39,22 +74,38 @@ func Eval(ast *Node) int {
 		result = eval(string(op), left, right)
 		return result
 	} else if isAssign(ast.root) {
-		AddToTable(string(ast.left.root.Value), toInt(ast.right.root.Value))
-		return toInt(ast.right.root.Value)
+		AddToTable(string(ast.left.root.Value), toNumber(ast.right.root.Value))
+		return toNumber(ast.right.root.Value)
 	}
-	return toInt(ast.root.Value)
+	return toNumber(ast.root.Value)
 }
 
-func eval(op string, left, right int) int {
+func eval(op string, left, right Number) Number {
+
+    if isAllInt(left, right){
+        v_left, v_right := intValue(left, right)
+        switch{
+        case op == "+":
+            return v_left + v_right
+        case op == "-":
+            return v_left - v_right
+        case op == "*":
+            return v_left * v_right
+        case op =="/":
+            return v_left / v_right
+        }
+        panic("not supported")
+    }
+    v_leftf, v_rightf := float64Value(left, right)
 	switch {
 	case op == "+":
-		return left + right
+		return v_leftf + v_rightf
 	case op == "-":
-		return left - right
+		return v_leftf - v_rightf
 	case op == "*":
-		return left * right
+		return v_leftf * v_rightf
 	case op == "/":
-		return left / right
+		return v_leftf / v_rightf
 	}
 
 	panic("not supported op " + string(op))
@@ -67,6 +118,34 @@ func toInt(val []byte) int {
 		result = result*factor + int(v-'0')
 	}
 	return int(result)
+}
+
+func toFloat(val []byte) float64{
+    result := 0.0
+    factor := 10.0
+    for _, v :=range val{
+        if !isDot(v){
+            if factor == 10.0{
+                result = result*factor + float64(v-'0')
+            }else {
+                result = result + float64(v-'0') * factor
+            }  
+        }else{
+            factor = 0.1
+        }
+
+    }
+    return float64(result)
+}
+
+func toNumber(val []byte) Number{
+    if isInt(val){
+        return toInt(val)
+    }else if isFloat(val){
+        return toFloat(val)
+    }else{
+        panic(string(val) + "not supported")
+    }
 }
 
 func AddToTable(key string, v interface{}) {
